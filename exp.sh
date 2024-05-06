2af3c7fbb5b4b391a9f93b88d64a3f27a3cbc605
XuWang <xu.wang@maastrichtuniversity.nl>
2024-05-07 00:01:00 +0200

    update exp shell

--- exp.sh
+++ exp.sh
@@ -2,16 +2,17 @@
 
 for exp in "$@" 
 do
-    for i in {1..1}; do python RecBole/bole_exp.py --model=CKE --dataset=$exp --config_files=config.yaml; done
-    for i in {1..1}; do python RecBole/bole_exp.py --model=CFKG --dataset=$exp --config_files=config.yaml; done
-    for i in {1..1}; do python RecBole/bole_exp.py --model=KGAT --dataset=$exp --config_files=config.yaml; done
-    for i in {1..1}; do python RecBole/bole_exp.py --model=KGCN --dataset=$exp --config_files=config.yaml; done
-    for i in {1..1}; do python RecBole/bole_exp.py --model=KGIN --dataset=$exp --config_files=config.yaml; done
-    for i in {1..1}; do python RecBole/bole_exp.py --model=KGNNLS --dataset=$exp --config_files=config.yaml; done
-    for i in {1..1}; do python RecBole/bole_exp.py --model=KTUP --dataset=$exp --config_files=config.yaml; done
-    for i in {1..1}; do python RecBole/bole_exp.py --model=MKR --dataset=$exp --config_files=config.yaml; done
-    for i in {1..1}; do python RecBole/bole_exp.py --model=RippleNet --dataset=$exp --config_files=config.yaml; done
-
+    cd RecBole
+    for i in {1..1}; do python bole_exp.py --model=CKE --dataset=$exp --config_files=config.yaml; done
+    for i in {1..1}; do python bole_exp.py --model=CFKG --dataset=$exp --config_files=config.yaml; done
+    for i in {1..1}; do python bole_exp.py --model=KGAT --dataset=$exp --config_files=config.yaml; done
+    for i in {1..1}; do python bole_exp.py --model=KGCN --dataset=$exp --config_files=config.yaml; done
+    for i in {1..1}; do python bole_exp.py --model=KGIN --dataset=$exp --config_files=config.yaml; done
+    for i in {1..1}; do python bole_exp.py --model=KGNNLS --dataset=$exp --config_files=config.yaml; done
+    for i in {1..1}; do python bole_exp.py --model=KTUP --dataset=$exp --config_files=config.yaml; done
+    for i in {1..1}; do python bole_exp.py --model=MKR --dataset=$exp --config_files=config.yaml; done
+    for i in {1..1}; do python bole_exp.py --model=RippleNet --dataset=$exp --config_files=config.yaml; done
+    cd ..
     for i in {1..1}; do python transe_lp.py $exp; done
     for i in {1..1}; do python transe_lp.py $exp; done
     for i in {1..1}; do python transe_lp.py $exp; done
